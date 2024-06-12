fedfc3a502a54b6ee281096efa32bc19d726683c
1BB3 <sanjeev1bb3@protonmail.com>
2024-06-12 11:42:30 +0545

    included placeholders in new thau form

--- components/thauharu/NewThauForm.js
+++ components/thauharu/NewThauForm.js
@@ -32,15 +32,33 @@ function NewThauForm(props) {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Title</label>
-          <input type="text" required id="title" ref={titleInputRef} />
+          <input
+            type="text"
+            required
+            id="title"
+            ref={titleInputRef}
+            placeholder="Thau Title"
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Image</label>
-          <input type="url" required id="image" ref={imageInputRef} />
+          <input
+            type="url"
+            required
+            id="image"
+            ref={imageInputRef}
+            placeholder="Input image link (eg. https://xxxxx/xxx.jpeg)"
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" ref={addressInputRef} />
+          <input
+            type="text"
+            required
+            id="address"
+            ref={addressInputRef}
+            placeholder="Thau Address"
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
@@ -49,6 +67,7 @@ function NewThauForm(props) {
             required
             rows="5"
             ref={descriptionInputRef}
+            placeholder="Some info about the thau."
           ></textarea>
         </div>
         <div className={classes.actions}>
